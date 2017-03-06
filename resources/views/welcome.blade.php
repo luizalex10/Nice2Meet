9be7afcbc3cbbf40659df284d5d098e362cc5870
Matheus Galdino <barbaromatrix@hotmail.com>
2017-03-06 12:31:35 +0000

    Testando clearDB

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 - {{ parse_url(getenv("CLEARDB_DATABASE_URL"))['host'] }}</div>
             </div>
         </div>
     </body>
